e3179c8c6b02c27d9b0a526b06ffda5ee3b33b5f
scao7 <32141118+scao7@users.noreply.github.com>
2019-02-27 23:34:21 -0600

    Update task1.scm

--- assignment1/task1.scm
+++ assignment1/task1.scm
@@ -1,22 +1,32 @@
-(define (cym in)
-	(define pi 3.14159265358979323846)
-	(define (cyan)
-		(int (*(sin(+(/(* pi in) 200) (/ pi 2))) 255))
-		;sin(((pi * in) / 200) * (pi * 2)) * 255 
+
+(define hexConvert "0123456789ABCDEF")
+(define (hex function)
+	(string+ (getElement hexConvert (int (/ function 16))) 
+		 (getElement hexConvert (int (% function 16)))
 	)
-	(define (yellow)
-		(int (* ( + (sin ( + (/ ( * pi in) 100 ) pi)) 1) 255))
-		
-	)
-	(define (magenta)
-	  	(int (* ( / (+(sin(+(/(* 3 pi in ) 200) (/ pi 2)))1)2)255))
-	)
-	(string+ (fmt "%02X" (cyan))(fmt "%02x" (yellow))(fmt "%02x"(yellow))(fmt "%02x" (magenta)))
-	(inspect (cyan))
-	(inspect (yellow))
-	(inspect (magenta))
 )
 
+(define (cym in)
+	(define pi 3.14159265358979323846)
+
+	(define cyan 
+		 (int (*(sin(+(/(* pi in) 200) (/ pi 2))) 255))
+	)
+		;sin(((pi * in) / 200) * (pi * 2)) * 255 
+
+	(define yellow
+		(int(* ( + (sin ( + (/ ( * pi in) 100 ) pi)) 1) 255))
+	)
+	(define magenta
+	  	(int (* ( / (+(sin(+(/(* 3 pi in ) 200) (/ pi 2)))1)2)255))
+	)
+	(println "(cyan " in ") is " cyan)
+	(println "(yellow " in ") is " yellow)
+	(println "(magenta " in ") is " magenta)
+	(println "(cym "  in ") is 0x" (hex cyan) (hex yellow) (hex magenta))
+)
 (define (main)
-  	(inspect (cym 0))
+  	(setPort (open(getElement ScamArgs 1) 'read))
+	(define arg (readExpr))
+  	(cym arg)
 )
