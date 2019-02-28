4eab28368e03e530e6811476a214d79a425e03b7
scao7 <32141118+scao7@users.noreply.github.com>
2019-02-28 00:50:08 -0600

    Update task2.scm

--- assignment1/task2.scm
+++ assignment1/task2.scm
@@ -1,3 +1,20 @@
-(define (mandelbrot arg)
+(define r 0.0)
+(define s 0.0)
+(define (mandelbrot threshold)
     
+    (if ( > (+ (* r r ) (* s s)) 4 ) ;if this always true diverge
+        (assign r (-( + (* r r ) (* s s) ) y))
+        (assign s (+ (* 2 r s) y))
+        (mandelbrot r)
+        (mandelbrot s)
+    )
+
+
+    (define (func x y)
+    
+    )
+)
+
+(define (main)
+    (mandelbrot 5)
 )
