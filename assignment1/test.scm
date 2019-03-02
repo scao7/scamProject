caceae2bba5f653eafc64c36aa9768f77cbca7ba
scao7 <32141118+scao7@users.noreply.github.com>
2019-03-02 14:36:18 -0600

    Update test.scm

--- assignment1/test.scm
+++ assignment1/test.scm
@@ -1,40 +1,11 @@
-(define pi 3.141592653589793238462643383279502884197169)
+(define (pascal row col left right)
+  (cond((< row col) #f)
+    	;((or (= 0 col) (= row col)) 1)
+		((= col 0) left)
+		((= row col )right)
+	(else (+ (pascal (- row 1) col left right)
+		 (pascal(- row 1)(- col 1) left right )))))
 
-(define (cym val)
-     ; calls cyan, yellow, magenta then returns their values as string with #
-        (string+ "#" (hex (cyan val)) (hex (yellow val)) (hex (magenta val))); add hex calls
-	   )
-
-(define (cyan val)
-     ; unModVal = cos((val*200/pi))
-        ; return unModVal *255
-	   (int (* 255 (cos (/ (* (real val) pi) 200))))
-	      )
-
-(define (yellow val)
-     ; unModVal = -sin(x) + 1
-        (int (* 255 (+ 1 (* -1 (sin (* pi .01 val))))))
-	   )
-
-(define (magenta val)
-     ; unModVal = cos(x) + 1 
-        (int (/ (* 255 (+ 1 (cos (/ (* 3 pi (real val)) 200)))) 2))
-	   )
-
-(define (run3) 
-     (exprTest (cym 0) "#FFFFFF")
-        (exprTest (cym 100) "#00FF7F") 
-	   )
-
-(define hexConv "0123456789ABCDEF")
-
-(define (hex function)
-     ; this function calls cyan, yellow, or magenta and then converts the return value to hex
-        ; and returns a 2 digit string of that hex
-	   (string+ (getElement hexConv (int (/ function 16))) 
-		                (getElement hexConv (int (% function 16))))
-	      )
-(define (main) 
-     (println(cym 0) "#FFFFFF")
-        (println (cym 100) "#00FF7F") 
-	   )
+(define (main)
+  (inspect (pascal 6 5 2 4))
+  )
