5525e9422129e1c2ba39944f6e9f3f979221a907
scao7 <32141118+scao7@users.noreply.github.com>
2019-03-03 11:15:22 -0600

    Update task9i.scm

--- assignment1/task9i.scm
+++ assignment1/task9i.scm
@@ -1,18 +1,19 @@
+;assue iterative way is allow to use while loop
 (define (ramanujani depth)
-  (define (iter store src)                 ; define an iterator function
-    (if (> src 1)                          ; we're working from the depth, up
-      (iter                                ; iterate, and: 
-        (* src (sqrt (+ (+ src 5.0) store))) ;   calculate new store w/ old store
-        (- src 1.0)                          ;   decrease the depth by 1
-        )
-      store                                ; once we hit min depth, return store
-      )
-    )
-  (iter 0 (+ 1 depth)) ; call iterator with (depth + 1)
-  )                    ; (because depth of 0 doesn't make sense to the maths)
+	(define i 0)
+	(define result 0.0)
+	(while (< i depth)
+		(set! result (sqrt (+ 6 i)))
+		(set! result (* result (+ i 1)))
+		(set! i (+ i 1))
+	)
+	result
+)                  
 
 (define(main) 
 	(setPort (open (getElement ScamArgs 1) 'read))
 	(define arg (readExpr))
-	(println "(ramanujani " arg ") is " (fmt "%.25f" (ramanujani arg)))
+	(println "(ramanujani " arg ") is " (fmt "%.25f" (ramanujani (+ arg 1))) ")")
+	
+	;change format to pass test case
 )
