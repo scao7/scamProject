5ad435c526ad0dc5d93a255370cd12e341e3d48c
scao7 <32141118+scao7@users.noreply.github.com>
2019-03-02 22:19:53 -0600

    Update task6.scm

--- assignment1/task6.scm
+++ assignment1/task6.scm
@@ -1,31 +1,36 @@
 (define (w f i)
+	(define (iter i)
 	(cond 
 		((= i 0) (f i))
-		(else ( 
+		(else
 			( / 
 				( - (*(s f (+ i 1)) (s f (- i 1))) (sqrt (s f i)) )
 
 				(+ (- (s f (+ i 1)) (* 2 (s f i))) ((s f (- i 1))))
 				
 			)
- 		)
+		)
+	)
 	)
 )
 
 (define (s f n)
-	(define i 0)
 	(define (iter i)
-		(f i)
 		
 		(if(<= i n)
 			(iter (+ i 1))
 		)
+		(+ i (f i))
 	)
-	(iter n)
+	(iter 0)
 )
 
 (define (main)
 	(setPort (open (getElement ScamArgs 1) 'read))
-	(define arg (readExpr))
-	(inspect (s arg 0))
+	(define arg (eval (readExpr) this))
+	(define arg1 (eval (readExpr) this))
+	
+	(inspect arg)
+	(inspect arg1)
+	(inspect (s arg arg1))
 )
