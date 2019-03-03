8de42454be8516b1b0afab078659291f09f4a800
scao7 <32141118+scao7@users.noreply.github.com>
2019-03-03 15:40:04 -0600

    Update task9i.scm

--- assignment1/task9i.scm
+++ assignment1/task9i.scm
@@ -1,19 +1,25 @@
 ;assue iterative way is allow to use while loop
 (define (ramanujani depth)
-	(define i 0)
+	(define i depth)
 	(define result 0.0)
-	(while (< i depth)
-		(set! result (sqrt (+ 6 i)))
+	(define (iter i) 
+		;(>= i 0)	
+		(set! result (+ result (+ 6 i) ) )
+		(set! result (sqrt result))
 		(set! result (* result (+ i 1)))
-		(set! i (+ i 1))
+		(set! i (- i 1))
+		(if (>= i 0)
+			(iter i)
+		)
 	)
+	(iter depth)
 	result
 )                  
 
 (define(main) 
 	(setPort (open (getElement ScamArgs 1) 'read))
 	(define arg (readExpr))
-	(println "(ramanujani " arg ") is " (fmt "%.25f" (ramanujani (+ arg 1))) ")")
+	(println "(ramanujani " arg ") is " (fmt "%.25f" (ramanujani  arg)) ")")
 	
 	;change format to pass test case
 )
