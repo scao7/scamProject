aefcd688c617d74aa890878bb623bec948e3017b
scao7 <32141118+scao7@users.noreply.github.com>
2019-03-02 21:24:06 -0600

    Update task2.scm

--- assignment1/task2.scm
+++ assignment1/task2.scm
@@ -1,21 +1,40 @@
-(define r 0.0)
-(define s 0.0)
-(define n 0)
-(define (mandelbrot threshold)  
-    (define (testFunc x y)
-    	(define (iter x y n)
-   		(if (and ( > (+ (* r r ) (* s s)) 4 ) (< n  threshold))
-		   ;if this always true diverge
-       			(set! r (+( - (* r r ) (* s s) ) x))
-		        (set! s (+ (* 2 r s) y))
-			n
-			(iter x y (+ n 1))
-	 	)
-		(println "loop")
-   	 )
+(define (mandelbrot threshold)
+  ; takes point (x, y) and checks if it
+  ; is within the mandlebrot set with threshold
+  ; of certainty
+  (lambda (x y)
+    ; recursive function to check divergence
+    (define (iter r s t)
+      ; check for threshold reached
+      (if (= t threshold) 
+        0
+        ; check for divergence
+        (if (< 2 (sqrt (+ (* r r) (* s s))))
+          ; if diverges, return t (the number of iterations checked)
+          t
+          ; otherwise, iterate one level deeper
+          (iter 
+            ; r value
+            (+ x (- (* r r) (* s s)))
+            ; s value
+            (+ y (* r s 2))
+            ; t value
+            (+ 1 t)
+            )
+          )
+        )
+      )
+    ; call with default values 0.0 for r and 0.0 for s
+    (iter 0.0 0.0 0)
     )
-)
+  )
+
+; (0, 0) should yeild 0
 (define (main)
-    (inspect ((mandelbrot 100)3 9 ))
-    (inspect (<(+(* r r) (* s s) ) 4))
-)
+  (setPort (open (getElement ScamArgs 1) 'read))
+  (define arg (readExpr))
+  (define arg1 (readExpr))
+  (define arg2 (readExpr))
+  (println "((mandelbrot " arg ") " arg1 arg2 ")) is " ((mandelbrot arg) arg1 arg2)) 
+  ;(inspect ((mandelbrot arg) arg1 arg2))
+  )
