1b8702bc68e39f920f872de2aa2111093b7fb7f4
scao7 <32141118+scao7@users.noreply.github.com>
2019-03-07 08:31:28 -0600

    Update task7.scm

--- assignment1/task7.scm
+++ assignment1/task7.scm
@@ -1,81 +1,63 @@
 ;task 7
 ; returns a number that is twice n
 (define (double n) (+ n n))
-
-; returns a number that is one half n
-(define (halve n)
-	(define (iter i) 
-	  	;(set! i (+ i i))
+(define (pow2 n)
+	(cond 
+	((= n 0 ) 1)
+	(else
+	(define (iter total i)
+		(if(< (+ i 1) n)
+			(iter (+ total total) (+ i 1))
+			total
+		)
+	)
+	(iter 2 0)
+	)
+	)
+)
+(define (halve x)
+	(define (outer d i)
+		(define ( inner a b)
+			;(inspect a)
+			;(inspect b)
+			(if(and (!= a x) (!= a (- x 1)))
+				(inner (- a 2) ( - b 1)) 
+				b
+			)
+		)
 		i
-		(if (< (+ i i) n)
-			(iter (+ i i))
-			i
-		  )
-	 )
-	(iter 1)
-  )
-  
-  
- ; (define (halve x)
-  ;  (define (worker x c t)
-   ;     (cond
-    ;        ( (<= (- x c) 0)
-     ;           t
-      ;          )
-       ;     ( (> (double (double c)) x)
-        ;        (worker (- x (double c)) 1 (+ t c))
-         ;       )
-          ;  ( (< (double c) x)
-           ;     (worker x (double c) t)
-           ;     )
-           ; )
-       ; )
-      ;  (worker x 1 0)
-   ; )
+		(if(< d x) 
+			(outer (double d) (+ i i))
+			(inner (- d 2) (- i 1))
+		)
+		
+	)
+	(outer 2 1)
+)
 
 (define (div2? n) (= (halve n) (halve (+ n 1))))
 
-;(define (ethiop x y)
-;  (define (iter total a b) 
-;    (if (>= b 1)              
-;      (iter                   
-;        (+ total            
-;          (if (div2? b) 0 a) 
-;          )                   
-;        (double a)           
- ;       (halve b)             
- ;       )
- ;     total ;
- ;     )
- ;   )
- 
-;    (iter 0 x y)
-;  )
-
-(define (ethiop x y )
-	(define i 0)
-	(define result 0)
-	(define (iter a b)
-		(set! result (+ result (if (div2? b) 0 a)))
-		(set! a (double a))
-		(set! b (halve b))
-		;(println " 1 ")
-		(if(>= b 1)
-		(iter a b)
-		)
-		;(println " 2 ")
-	)
-	(iter x y)
-	result
+(define (ethiop x y)
+  (define (iter total a b)  
+    (if (>= b 1)             
+      (iter (+ total (if (even? b) 0 a) )                   
+        (double a)           
+        (halve b)            
+      )
+      total
+    )
+  )
+    (iter 0 x y)
 )
 
 (define (main)
-(setPort (open (getElement ScamArgs 1) 'read))
-(define arg1 (readExpr))
-(define arg2 (readExpr))
-(inspect (halve 10))
-(println "(halve " arg1 ") is " (halve arg1))
-(println "(double " arg1 ") is " (double arg1))
-(println "(div2? " arg1 ") is " (div2? arg1))
-(println "(ethiop " arg1 " " arg2 ") is " (ethiop arg1 arg2))
+	(setPort (open (getElement ScamArgs 1) 'read))
+	(define arg1 (readExpr))
+	(define arg2 (readExpr))
+	;(inspect (pow2 3))
+	;(inspect (halve 960))
+	(println "(halve " arg1 ") is " (halve arg1))
+	(println "(double " arg1 ") is " (double arg1))
+	(println "(div2? " arg1 ") is " (div2? arg1))
+	(println "(ethiop " arg1 " " arg2 ") is " (ethiop arg1 arg2))
 )
