2ba39efd84f6c6185387ac74c9cfb1f434837710
scao7 <32141118+scao7@users.noreply.github.com>
2019-03-10 10:38:42 -0500

    Create task1.scm

---
+++ assignment2/task1.scm
@@ -0,0 +1,17 @@
+;for-loop
+(define (range start end n)
+	
+	(range start end n)
+)
+
+(define (for-loop range function)
+	
+	
+	(range) 
+)
+
+(define (main)
+
+(define env this)
+    (for-loop (range 0 10 1) (lambda (i) (inspect i)))
+)
