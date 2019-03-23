1a8d1fdb0023c432356ac2df936a74dbe1a7f822
scao7 <32141118+scao7@users.noreply.github.com>
2019-03-23 11:48:47 -0500

    Update task9i.scm

--- assignment1/task9i.scm
+++ assignment1/task9i.scm
@@ -1,18 +1,15 @@
 ;assue iterative way is allow to use while loop
 (define (ramanujani depth)
-	(define i depth)
+	;(define i (+ depth 1))
 	;(define result 0.0)
 	(define (iter result i) 
-		;(>= i 0)	
-		;(set! result )
-		;(set! i (- i 1))
 		(if (>= i 0)
 			(iter (* (sqrt (+ result (+ 6 i) )) (+ i 1)) (- i 1))
 			result
 		)
 		;result
 	)
-	(iter 0.0 depth)
+	(iter 0 depth)
 )                  
 
 (define(main) 
