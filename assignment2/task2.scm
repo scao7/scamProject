733b5dbad446cb4fd94b23af961199faf2d60009
scao7 <codescao7@gmail.com>
2019-03-23 22:31:04 -0500

    1-5

--- assignment2/task2.scm
+++ assignment2/task2.scm
@@ -1,11 +1,23 @@
-(define (curry f )
-	(define (lambda a)
-		(define (lambda b)
-			(define (lambda c)
-				(define (lambda d)
-					(f a b c d)
-				)
-			)
-		)
-	)	
+(define (curry f @)
+    (define original @)
+    (define (function @)
+        (define (buildArgumentList finalList outer inner)
+            (cond 
+             ((eq? inner nil) (reverse (append outer finalList)))
+             ((eq? (car outer) 'MISSING) (buildArgumentList (cons (car inner) finalList)  (cdr outer) (cdr inner)))
+             (else (buildArgumentList (cons (car outer) finalList)  (cdr outer) inner))
+            )
+        )
+
+        (apply f (buildArgumentList nil original @))
+    )
+)
+
+(define (main)
+  (define (f x y z) (+ x y z))
+    (define a 1)
+    (define b 2)
+    (define c 3)
+
+    
 )
